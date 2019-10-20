9b09c8e0047ec232dc270dfe9766bea5d3e0eed0
fsajadi <s.sfatemeh@gmail.com>
2019-10-20 02:35:22 -0400

    animationframe added

--- src/app/count-down/count-down.component.ts
+++ src/app/count-down/count-down.component.ts
@@ -5,36 +5,28 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './count-down.component.html',
   styleUrls: ['./count-down.component.css']
 })
-export class CountDownComponent implements OnInit {
+export class CountDownComponent {
+  date = 0;
+  calcDays = 0;
+  calcHours = 0;
+  calcMins = 0;
+  calcSecs = 0;
 
-  date=0;
-  end = 0
-  timerStart = new Date().getTime()
-  tRemaining = 0
-  calcDays = 0
-  calcHours = 0
-  calcMins = 0
-  calcSecs = 0
-  rDay = 0
-  rHour = 0
-  
-
-  intervalId = window.setInterval(() => {
-    let end = new Date(this.date).getTime();
-    let start = this.timerStart;
-    this.tRemaining = (end - start)/1000;
-    this.timerStart = new Date().getTime();
-    this.calcDays = Math.floor(this.tRemaining / (60 * 60 * 24));
-    this.rDay = this.tRemaining % (60 * 60 * 24)
-    this.calcHours =  Math.floor(this.rDay / (60*60))
-    this.rHour = this.rDay %(60*60)
-    this.calcMins =  Math.floor(this.rHour / (60))
-    this.calcSecs =  Math.floor(this.rHour % (60))
-
-  }, 1000);
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    requestAnimationFrame(this.updateClock);
   }
 
+  updateClock = () => {
+    const end = new Date(this.date).getTime();
+    const start = Date.now()
+    const tRemaining = (end - start) / 1000;
+    const rDay = tRemaining % (60 * 60 * 24);
+    const rHour = rDay % (60 * 60);
+
+    this.calcDays = Math.floor(tRemaining / (60 * 60 * 24));
+    this.calcHours = Math.floor(rDay / (60 * 60));
+    this.calcMins = Math.floor(rHour / (60));
+    this.calcSecs = Math.floor(rHour % (60));
+    requestAnimationFrame(this.updateClock);
+  }
 }
